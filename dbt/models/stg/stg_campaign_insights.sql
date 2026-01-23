4b58e20e2b6c5a06f9eda2fbe5607ecba3a8be51
Nhat Quang Nguyen <nhatquang.nguyen.129@gmail.com>
2026-01-23 14:56:39 +0700

    use jinja template to fix wildcard table suffix error

--- dbt/models/stg/stg_campaign_insights.sql
+++ dbt/models/stg/stg_campaign_insights.sql
@@ -1,9 +1,31 @@
-{{ 
-  config(
-    materialized = 'ephemeral'
-  ) 
-}}
+{{ config(materialized='ephemeral') }}
 
+{% set raw_schema = var('company') ~ '_dataset_google_api_raw' %}
+{% set table_prefix = var('company') ~ '_table_google_' ~ var('department') ~ '_' ~ var('account') ~ '_campaign_' %}
+
+{% set tables_query %}
+    select table_name
+    from `{{ var('project') }}.{{ raw_schema }}.INFORMATION_SCHEMA.TABLES`
+    where table_name like '{{ table_prefix }}m______'
+{% endset %}
+
+{% set results = run_query(tables_query) %}
+{% set table_names = results.columns[0].values() if results is not none else [] %}
+
+{% if table_names | length == 0 %}
+    select
+        null as customer_id,
+        null as campaign_id,
+        null as date,
+        null as impressions,
+        null as clicks,
+        null as cost,
+        null as conversions,
+        null as conversion_value
+    where false
+{% else %}
+
+{% for table_name in table_names %}
 select
     customer_id,
     campaign_id,
@@ -13,7 +35,8 @@ select
     cost,
     conversions,
     conversion_value
+from `{{ var('project') }}.{{ raw_schema }}.{{ table_name }}`
+{% if not loop.last %} union all {% endif %}
+{% endfor %}
 
-from `{{ var('project') }}.{{ var('company') }}_dataset_google_api_raw.{{ var('company') }}_table_google_{{ var('department') }}_{{ var('account') }}_campaign_*`
-
-where REGEXP_CONTAINS(_table_suffix, r'^m\d{6}$')
+{% endif %}
