159ca76e6fa3884fe3eb7039b44bb5c546a38d2c
Nhat Quang Nguyen <nhatquang.nguyen.129@gmail.com>
2026-01-23 14:28:23 +0700

    add account to vars

--- dbt/models/stg/stg_campaign_insights.sql
+++ dbt/models/stg/stg_campaign_insights.sql
@@ -15,7 +15,6 @@ select
     conversions,
     conversion_value
 
-from `{{ env_var('PROJECT') }}.{{ env_var('COMPANY') }}_dataset_google_api_raw.
-      {{ env_var('COMPANY') }}_table_google_{{ env_var('DEPARTMENT') }}_{{ env_var('ACCOUNT') }}_campaign_*`
+from `{{ env_var('PROJECT') }}.{{ env_var('COMPANY') }}_dataset_google_api_raw.{{ env_var('COMPANY') }}_table_google_{{ env_var('DEPARTMENT') }}_{{ env_var('ACCOUNT') }}_campaign_*`
 
 where _table_suffix like 'm%'
