951291a54f7ce748ff959f3e4da85cd5959b1daf
Nhat Quang Nguyen <nhatquang.nguyen.129@gmail.com>
2026-02-24 16:08:09 +0700

    added month year

--- dbt/models/stg/stg_campaign_insights.sql
+++ dbt/models/stg/stg_campaign_insights.sql
@@ -29,12 +29,17 @@
     select
         cast(null as string)  as customer_id,
         cast(null as string)  as campaign_id,
-        cast(null as date)    as date,
+
         cast(null as int64)   as impressions,
         cast(null as int64)   as clicks,
         cast(null as numeric) as spend,
         cast(null as int64)   as conversions,
-        cast(null as numeric) as conversion_value
+        cast(null as numeric) as conversion_value,
+
+        cast(null as date)    as date,    
+        cast(null as int64)   as year,
+        cast(null as string)  as month
+
     where false
 
 {% else %}
@@ -44,7 +49,9 @@
 select
     customer_id,
     campaign_id,
-    date,
+    DATE(date) as date,
+    month,
+    year,
     impressions,
     clicks,
     spend,
