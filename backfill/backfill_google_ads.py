dea160e2f0a927671db46da136f06cc752d4d9a5
Nhat Quang Nguyen <nhatquang.nguyen.129@gmail.com>
2026-03-12 16:16:05 +0700

    fixed bool error

--- backfill/backfill_google_ads.py
+++ backfill/backfill_google_ads.py
@@ -192,7 +192,7 @@ def backfill():
 
 # Execute DAGs
     dags_google_ads(
-        google_ads_credentials==google_ads_credentials,
+        google_ads_credentials=google_ads_credentials,
         customer_id=google_customer_id,
         start_date=start_date,
         end_date=end_date
