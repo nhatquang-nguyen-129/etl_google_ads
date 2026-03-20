60abd3cc8a2f4a79ab594c0669a447b1f5dd6010
Nhat Quang Nguyen <nhatquang.nguyen.129@gmail.com>
2026-03-20 09:25:26 +0700

    generated credentials payload to cli

--- auth/google_ads_oauth.py
+++ auth/google_ads_oauth.py
@@ -14,10 +14,10 @@ SCOPES = ["https://www.googleapis.com/auth/adwords"]
 GCP_OAUTH2_CLIENT_SECRET_FILE = Path(__file__).resolve().parent / "oauth2_desktop_client.json"
 
 # Put your Google Ads MCC Developer Token here
-GOOGLE_ADS_MCC_DEVELOPER_TOKEN = "EWM3cbV4ggotWwRixrHg_w"
+GOOGLE_ADS_MCC_DEVELOPER_TOKEN = "PUT-YOUR-GOOGLE-ADS-DEVELOPER-TOKEN-HERE"
 
 # Put your Google Ads MCC numbers only no dashes customer ID here
-GOOGLE_ADS_MCC_CUSTOMER_ID = "3394321584"
+GOOGLE_ADS_MCC_CUSTOMER_ID = "PUT-YOUR-GOOGLE-ADS-MCC-NUMBERS-WITH-NO-DASHES-HERE"
 
 def internal_bootstrap_oauth():
     """
